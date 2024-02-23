550547dfc9d057477648abeacf9961de281d7b0b
Jubayer Fahim <156297223+jubayerfahim2002@users.noreply.github.com>
2024-02-24 00:22:21 +0600

    Add files via upload

---
+++ omni food/vector/js/slide.js
@@ -0,0 +1,28 @@
+
+  // setInterval(()=>{
+  //   if(document.getElementsByClassName('naviget-img-bx')[0].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[1].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[0].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[1].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[2].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[1].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[2].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[3].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[2].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[3].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[4].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[3].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[4].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[5].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[4].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[5].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[6].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[5].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[6].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[7].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[6].classList.remove('active');
+  //     }else if(document.getElementsByClassName('naviget-img-bx')[7].classList.contains('active')){
+  //       document.getElementsByClassName('naviget-img-bx')[0].classList.add('active');
+  //       document.getElementsByClassName('naviget-img-bx')[7].classList.remove('active');
+  //     }
+  // },2000)
